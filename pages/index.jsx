7caf699059b619435f1263b2df219263d3bdcbb1
JobDon <jobie@commandcentre.it>
2025-08-06 23:17:52 +0200

    Add files via upload

---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ minHeight: "100vh", backgroundColor: "#000", color: "#fff", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <h1>Welcome to Command Centre IT</h1>
+    </div>
+  )
+}
